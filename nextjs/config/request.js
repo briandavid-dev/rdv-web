97bcb3f7bf0a195de5d8e10c2e3446a41a49904d
bob <brayad@gmail.com>
2021-04-24 15:43:47 -0400

    config token

--- nextjs/config/request.js
+++ nextjs/config/request.js
@@ -7,4 +7,14 @@ const config = {
 };
 const request = axios.create(config);
 
+request.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
 export default request;
